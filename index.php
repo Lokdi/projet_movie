2b1ae50beb1fc2b9f9ee8d27f1500377034178f0
lokdi <loic.gourdeau@icloud.com>
2017-12-08 10:17:01 +0100

    commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include('inc/fonction.php'); ?>
+<?php $title = ''; ?>
+<?php $titreH1 = ''; ?>
+<?php include('inc/data.php') ?>
+
+
+<?php include ('inc/header.php'); ?>
+
+<?php include ('inc/footer.php'); ?>
