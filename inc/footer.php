90f752409d4344d6e5c14885d593a62e7c0159af
franck <thekingofpizzas@gmail.com>
2017-12-11 14:15:32 +0100

    francky

--- inc/footer.php
+++ inc/footer.php
@@ -9,20 +9,20 @@
     <script type="text/javascript" src="assets/js/jquery-ui.min.js"></script>
 
     <script type="text/javascript" src="assets/js/main.js">
-    var btn = document.querySelector('input');
-    var txt = document.querySelector('p');
-
-    btn.addEventListener('click', updateBtn);
-
-    function updateBtn() {
-        if (btn.value === 'Démarrer la machine') {
-            btn.value = 'Arrêter la machine';
-            txt.textContent = 'La machine est démarrée !';
-        } else {
-            btn.value = 'Démarrer la machine';
-            txt.textContent = 'La machine est arrêtée.';
-        }
-    }
+    // var btn = document.querySelector('input');
+    // var txt = document.querySelector('p');
+    //
+    // btn.addEventListener('click', updateBtn);
+    //
+    // function updateBtn() {
+    //     if (btn.value === 'Démarrer la machine') {
+    //         btn.value = 'Arrêter la machine';
+    //         txt.textContent = 'La machine est démarrée !';
+    //     } else {
+    //         btn.value = 'Démarrer la machine';
+    //         txt.textContent = 'La machine est arrêtée.';
+    //     }
+    // }
 
 
 
